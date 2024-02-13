c4b30ea5a03612324bd3c63de118881496b8a87b
Aleksandar <a.doklyov@gmail.com>
2024-02-13 19:22:26 +0200

    Added Game.h and Task.h, to check folder composition.

---
+++ SDL_images_homework/SDL_Task7_homework/Game.h
@@ -0,0 +1,51 @@
+#ifndef GAME_H
+#define GAME_H
+#include <SDL.h>
+
+using namespace std;
+
+class Game {
+
+public:
+
+    //Constructor and destructor
+    Game();
+    ~Game();
+
+    //Initialise the game window, renderer and game state
+    bool init(const char* title, int xpos, int ypos, int width, int height, int flags);
+    //Render the game objects
+    void render();
+    //Update the game state and objects
+    void update();
+    //Processes events
+    void handleEvents();
+    //Cleans up the game
+    void clean();
+    //Checks if the game is running
+    bool isRunning();
+
+private:
+
+    //SDL window and renderer
+    SDL_Window* gameWindow = NULL;
+    SDL_Renderer* gameRenderer = NULL;
+    //Game running state
+    bool running;
+
+    //SDL texture
+    SDL_Texture* texture;
+    //SDL rectangles for source and destination
+    SDL_Rect sourceRectangle;
+    SDL_Rect destinationRectangle;
+
+    //Speed of the picture
+    int picSpeed;
+    //Position of the picture
+    SDL_Point picPos;
+    //Mouse click check
+    bool isMousePressed;
+
+};
+
+#endif
