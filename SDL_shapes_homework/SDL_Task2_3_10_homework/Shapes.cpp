aa4474253c82f51326a42d271770ae5cff5ab502
Aleksandar <a.doklyov@gmail.com>
2024-02-15 10:48:39 +0200

    Task 10 added, next one is 6.

--- SDL_shapes_homework/SDL_Task2_3_10_homework/Shapes.cpp
+++ SDL_shapes_homework/SDL_Task2_3_10_homework/Shapes.cpp
@@ -30,3 +30,20 @@ void Shapes::drawHexagon(SDL_Renderer* gameRenderer, int x, int y, int side) {
 		SDL_RenderDrawLine(gameRenderer, x + R * cos(angle1), y + R * sin(angle1), x + R * cos(angle2), y + R * sin(angle2));
 	}
 }
+
+//Method to draw an ellipse
+void Shapes::drawEllipse(SDL_Renderer* gameRenderer, int x, int y, int a, int b) {
+
+	double angle = 0;
+	double x1 = x + a * cos(angle);
+	double y1 = y + b * sin(angle);
+
+	for (int i = 1; i < 360; i++) {
+		angle = i * M_PI / 180;
+		double x2 = x + a * cos(angle);
+		double y2 = y + b * sin(angle);
+		SDL_RenderDrawLine(gameRenderer, x1, y1, x2, y2);
+		x1 = x2;
+		y1 = y2;
+	}
+}
--- SDL_shapes_homework/SDL_Task2_3_10_homework/Shapes.h
+++ SDL_shapes_homework/SDL_Task2_3_10_homework/Shapes.h
@@ -9,7 +9,8 @@ public:
 	//Methods
 	static void drawPentagon(SDL_Renderer* gameRenderer, int x, int y, int r);
 	static void drawHexagon(SDL_Renderer* gameRenderer, int x, int y, int r);
-
+	static void drawEllipse(SDL_Renderer* gameRenderer, int x, int y, int rx, int ry);
+	static void drawEmptyRectangle(SDL_Renderer* gameRenderer, int x, int y, int w, int h, int thickness);
 };
 
 #endif
